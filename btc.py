e5bb3a82f1b25d4734cc63b5ae61c5e51aa33ea4
mete <metebunyaminn@gmail.com>
2020-05-31 18:33:56 +0300

    upload

---
+++ btc.py
@@ -0,0 +1,14 @@
+import requests
+import time
+import json
+
+old_price = 1
+while True:
+    url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
+    response = requests.get(url).json()
+    time_updated = response['time']['updated']
+    rate_price = float(response['bpi']['USD']['rate_float'])
+    ratio = (rate_price - old_price) / old_price * 100
+    print("\n\n" + time_updated + "\nbtc/dollar: " + str(rate_price) + "\nratio: %" + str(ratio))
+    old_price = rate_price
+    time.sleep(60)
